db1d6a4dae9165205de02ee66e978a324e5fa03a
polar0n <igariac@gmail.com>
2026-03-01 18:52:52 +0200

    Fixed missing log file

--- config/logging.py
+++ config/logging.py
@@ -1,9 +1,11 @@
 import os
 import logging
 from logging.handlers import RotatingFileHandler
+from pathlib import Path
 
 
 def setup_logger() -> logging.Logger:
+    Path("logs/main.log").touch()
     # Keep up to 5 logs of max size of 5 MB
     file_handler = RotatingFileHandler(
         filename="logs/main.log",
